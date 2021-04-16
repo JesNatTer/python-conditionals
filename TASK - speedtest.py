ef707b9cf0a4188bf0316cebd25b7ffd61448946
JesNatTer <82204857+JesNatTer@users.noreply.github.com>
2021-04-16 15:59:08 +0200

    Update TASK - speedtest.py

--- TASK - speedtest.py
+++ TASK - speedtest.py
@@ -1,7 +1,7 @@
 speed_limit = int(input("ENTER THE SPEED LIMIT OF ROAD:"))
 speed = int(input("ENTER YOUR DRIVING SPEED:"))
 points = (speed - speed_limit)/5
-if speed < 60:
+if speed <= 60:
     print("OK")
 elif speed > speed_limit:
     if points > 12:
