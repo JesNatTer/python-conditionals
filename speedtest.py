fd10c4ee497a390e4a8ee7a659afe05b62a0b57c
JesNatTer <jessenterblanche@gmail.com>
2021-04-16 15:25:30 +0200

    first commit

---
+++ speedtest.py
@@ -0,0 +1,10 @@
+speed_limit = int(input("ENTER THE SPEED LIMIT OF ROAD:"))
+speed = int(input("ENTER YOUR DRIVING SPEED:"))
+points = (speed - speed_limit)/5
+if speed < 60:
+    print("OK")
+elif speed > speed_limit:
+    if points > 12:
+        print("You going to jail lmao. Demerit Points:" + str(points))
+    else:
+        print("Demerit Points:" + str(points))
